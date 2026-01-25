6b71ee724e4cdf2105bd7a3e3d7396171093fd34
Chris Marwick <chris_marwick@hotmail.com>
2026-01-25 15:49:36 +1100

    fix unsupported tf parameter

--- terraform/main.tf
+++ terraform/main.tf
@@ -110,7 +110,6 @@ resource "aws_apigatewayv2_integration" "api_predict_integration" {
   api_id = aws_apigatewayv2_api.api_gateway.id
   integration_type = "AWS_PROXY"
   connection_type = "INTERNET"
-  content_handling_strategy = "CONVERT_TO_TEXT"
   integration_method = "POST"
   integration_uri = aws_lambda_function.func_predict.invoke_arn
 }
